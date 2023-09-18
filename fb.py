19de49b175da26483b387b96b871f00f5615a16a
LWaien <lawaien14@gmail.com>
2023-09-17 20:43:36 -0400

    first commit

---
+++ fb.py
@@ -0,0 +1,102 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import json
+import fb
+
+cred = credentials.Certificate("credentials.json")
+firebase_admin.initialize_app(cred, {'databaseURL':'https://concertrec-da7fc-default-rtdb.firebaseio.com/'})
+ref = db.reference()
+users = ref.child('users')
+
+def insertEmailInfo(spotify_user, email,fname,lname,maxdist,location):
+    keys = searchDb('spotify_user',spotify_user)
+    user_key = keys[0]
+    #referencing the user we want to add data for
+    user = users.child(user_key)
+    
+    user.update({
+        'email': email,
+        'fname': fname,
+        'lname': lname,
+        'maxdist': maxdist,
+        'location': location,
+        
+        })
+
+    return {'msg':'email info successfully added to the the database'},201
+
+def createNewUser(spotify_user):
+    users.push({
+        'email': '',
+        'spotify_user': spotify_user,
+        'fname': '',
+        'lname': '',
+        'maxdist': '',
+        'location': '',
+        'last_email': '',
+        'top_artists': '',
+        'libdata': ''
+        })
+
+
+def searchDb(search_key,search_value):
+
+    # Retrieve the entire dataset
+    user = users.get()
+
+    # Filter the data locally based on the key-value pair
+    if user:
+        result = {
+            key: value
+            for key, value in user.items()
+            if value.get(search_key) == search_value
+        }
+        key_ids = list(result.keys())
+        return key_ids
+    else:
+        return None
+
+def addSpotifyData(spotify_user,topartists,libdata,topsongs):
+    #search for user in db
+    print(f"Adding user data for {spotify_user}")
+    keys = searchDb('spotify_user',spotify_user)
+
+    if keys is None:
+        return {'msg':'Could Not Find User'},404
+    
+    #returns list of ids for applicable entries
+    user_key = keys[0]
+    #referencing the user we want to add data for
+    user = users.child(user_key)
+    user.update({'libdata':libdata,'top_artists':topartists,'top_songs':topsongs})
+    #print(user.get())
+    return 'Library data added',201
+
+def checkData(spotify_user):
+    keys = searchDb('spotify_user',spotify_user)
+    user = users.child(keys[0])
+
+    if user.get('libdata') is None and user.get('top_artists') is None:
+        #return false as in user data does not exist
+        return False
+    else:
+        return True
+
+def get_topartists(keys):
+    user_key = keys[0]
+    user = users.child(user_key)    
+    artist_data = user.get('email')
+    #artist data is first returned as a tuple so we're just going to reference it with index 0 and then reference 'top_artists' from the dictionary within it
+    artist_data = artist_data[0]['top_artists']['items']
+    return artist_data
+
+def getpreviousEmail(keys):
+    user_key = keys[0]
+    user = users.child(user_key)
+    try:
+        prev_email = user.get('previous_list')
+        prev_email = prev_email[0]['previous_list']
+        return prev_email
+    except:
+        return None
---
+++ main.py
@@ -0,0 +1,92 @@
+from flask import Flask, redirect, url_for, request,jsonify, make_response
+from urllib.parse import urlencode
+import fb
+
+
+CLIENT_ID = "32f3ca3f815c4b7f91335ffeb5d90f7d"
+CLIENT_SECRET = "3f882c04f6824a68b45b251ff922488a"
+
+
+app = Flask(__name__)
+
+
+@app.route("/topartists/<spotify_user>",methods=['GET'])
+def topartists(spotify_user):
+    #retrieving user key that we want to query
+    keys = fb.searchDb('spotify_user',spotify_user)
+    #passing key to retrieve top artists for the user 
+    top_artists = fb.get_topartists(keys)
+    #return user's top_artists
+    return top_artists
+
+@app.route("/previousEmail/<spotify_user>",methods=['GET'])
+def previousEmail(spotify_user):
+    #retrieving user key that we want to query
+    keys = fb.searchDb('spotify_user',spotify_user)
+    #passing key to retrieve top artists for the user 
+    previous_email = fb.getpreviousEmail(keys)
+    #return user's top_artists
+    return previous_email
+
+
+@app.route("/addEmailInfo",methods=['GET'])
+def addEmailInfo():
+    #collect data from request
+    spotify_user = request.args.get('spotify_user')
+    email = request.args.get('email')
+    first_name = request.args.get('fname')
+    last_name = request.args.get('lname')
+    max_distance = request.args.get('max_distance')
+    location = [request.args.get('location')]
+
+    #attempt to push to db
+    try:
+        response_msg,response_code = fb.insertEmailInfo(spotify_user, email,first_name,last_name,max_distance,location)
+        #if response code is 409 (user exists), front end should redirect to login
+        return make_response(response_msg,response_code)
+    except:
+        #return 404 if db system fails
+        return make_response({'msg':"Unable to connect to the database"},404)
+
+
+@app.route("/createNewUser",methods=['GET'])
+def createNewUser():
+    spotify_user = request.args.get('spotify_user')
+    try:
+        fb.createNewUser(spotify_user)
+        #if response code is 409 (user exists), front end should redirect to login
+        return make_response({'msg':'user was successfully added to the the database'},201)
+    except:
+        #return 404 if db system fails
+        return make_response({'msg':"Unable to connect to the database"},404)
+
+
+@app.route("/checkUser/<spotify_user>/",methods=['GET'])
+def checkUser2(spotify_user):
+    keys = fb.searchDb('spotify_user',spotify_user)
+    if keys:
+        return make_response({'msg':'User exists'},200)
+    else:
+        return make_response({'msg':'User not found'},404)
+
+   
+@app.route("/checkUser/<spotify_user>/<email>",methods=['GET'])
+def checkUser(spotify_user,email):
+    keys = fb.searchDb('spotify_user',spotify_user)
+    if keys:
+        return make_response({'msg':'User exists'},200)
+    
+    #print(email)
+    #first check if user exists
+    user_keys = fb.searchDb('email',email)
+    if user_keys:
+        #second, check if spotify data loaded. Give according resp so 
+        #front end can display
+        spotify_keys = fb.searchDb('spotify_user',spotify_user)
+        if spotify_keys:
+            return make_response({'msg':'User exists'},200)
+        else:
+            return make_response({'msg':'data still loading'},201)
+    else:
+        print('user does not exist')
+        return make_response({'msg':'User does not have an account'},404)
